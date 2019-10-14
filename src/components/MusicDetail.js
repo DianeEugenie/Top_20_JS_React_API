e3c3679708b37bbd0fc8d2222cd9bc68918027d7
Diane Jonker <Diane.eugenie.jonker@gmail.com>
2019-10-14 17:35:27 +0100

    Adds the album illustration and song as a link

--- src/components/MusicDetail.js
+++ src/components/MusicDetail.js
@@ -10,9 +10,16 @@ const MusicDetail = (props) => {
 
   return (
     <div className='music-detail'>
+      <div className='song-album'>
+      <img src={props.song['im:image'][2].label} alt={props.song['im:name']} />
+      <p><a href={props.song.link[1].attributes.href}>Give this tune a spin!</a></p>
+      </div>
+      <div className='song-details'>
       <h2>Coming in at Number {props.index + 1}</h2>
       <h3>The song: {props.song['im:name'].label}</h3>
       <h3>The artist: {props.song['im:artist'].label}</h3>
+      </div>
+
     </div>
   );
 
